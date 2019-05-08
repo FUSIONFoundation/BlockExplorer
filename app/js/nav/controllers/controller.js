1e36e9ce9bb1c90e133e52da08bc54413f1f128c
Joey Kheireddine <joey.kheireddine@outlook.com>
2019-05-08 14:17:46 +0200

    Fixed searchbar to top and hidden on mobile devices

--- app/js/nav/controllers/controller.js
+++ app/js/nav/controllers/controller.js
@@ -1,5 +1,17 @@
 let navController = function ($http, $scope, $location) {
     $scope.$location = $location;
+
+    $scope.search = function (){
+        let input = $scope.globalSearch;
+        let reg = new RegExp('^[0-9]*$');
+        if(reg.test(input)){
+            window.location.href = `./#!/block/${input}`;
+        }else if(input.indexOf('0x') == 0 && input.length == 42 && web3.utils.isAddress($scope.globalSearch)){
+                window.location.href = `./#!/address/${input}`;
+        } else if (input.indexOf('0x') == 0 && input.length == 66){
+                window.location.href = `./#!/transaction/${input}`;
+        }
+    }
 };
 
 export default navController;
