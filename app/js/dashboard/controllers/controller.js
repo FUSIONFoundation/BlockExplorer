5dcaa2efd7895401a50da6ae19822444a6157ae5
Joey Kheireddine <joey.kheireddine@outlook.com>
2019-05-08 13:58:13 +0200

    Finalized search funciton

--- app/js/dashboard/controllers/controller.js
+++ app/js/dashboard/controllers/controller.js
@@ -1,4 +1,4 @@
-let dashboardController = function ($http, $scope, $state) {
+let dashboardController = function ($http, $scope, $state, $location) {
     $scope.totalParticipants = 0;
     $scope.fsnPriceData = {};
     $scope.lastUpdated = moment(new Date().getTime()).format('LTS');
@@ -7,6 +7,18 @@ let dashboardController = function ($http, $scope, $state) {
     $scope.globalData = [];
     $scope.loading = false;
     $scope.totalTickets = 0;
+
+    $scope.search = function (){
+        let input = $scope.globalSearch;
+        let reg = new RegExp('^[0-9]*$');
+        if(reg.test(input)){
+            window.location.href = `./#!/block/${input}`;
+        }else if(input.indexOf('0x') == 0 && input.length == 42 && web3.utils.isAddress($scope.globalSearch)){
+                window.location.href = `./#!/address/${input}`;
+        } else if (input.indexOf('0x') == 0 && input.length == 66){
+                window.location.href = `./#!/transaction/${input}`;
+        }
+    }
 
     $scope.getLatestBlocks = function () {
         let recentBlocks = [];
