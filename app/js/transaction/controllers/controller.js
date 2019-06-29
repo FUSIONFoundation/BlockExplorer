c089768d5010b053058fe39159a65a1bca68c5e0
Joey Kheireddine <joey.kheireddine@outlook.com>
2019-06-29 12:47:57 +0200

    Added Make Swap details to Transaction page

--- app/js/transaction/controllers/controller.js
+++ app/js/transaction/controllers/controller.js
@@ -1,4 +1,4 @@
-import BigNumber from "bignumber.js";
+import BigNumber from 'bignumber.js';
 
 let transactionController = function ($http, $scope, $stateParams) {
     let transactionHash = $stateParams.transactionId;
@@ -6,12 +6,12 @@ let transactionController = function ($http, $scope, $stateParams) {
 
     $scope.returnDateString = function (posixtime, position) {
         let time = new Date(parseInt(posixtime) * 1000);
-        if (posixtime == 18446744073709552000 && position == "End") {
-            return "Forever";
+        if (posixtime == 18446744073709552000 && position == 'End') {
+            return 'Forever';
         }
-        if (position == "Start") {
+        if (position == 'Start') {
             if (posixtime == 0) {
-                return "Now";
+                return 'Now';
             }
             // if(posixtime < time && position == 'Start'){return 'Now';}
         }
@@ -19,7 +19,7 @@ let transactionController = function ($http, $scope, $stateParams) {
         let tDay = time.getUTCDate();
         let tYear = time.getUTCFullYear();
 
-        return window.months[tMonth] + " " + tDay + ", " + tYear;
+        return window.months[tMonth] + ' ' + tDay + ', ' + tYear;
     };
 
 
@@ -43,46 +43,61 @@ let transactionController = function ($http, $scope, $stateParams) {
             txExtraData2 = JSON.parse(r.data[0].data);
             txTransactionData = JSON.parse(r.data[0].transaction);
             data = {
-                from : tx.fromAddress,
-                to : tx.commandExtra3,
-                asset : '',
-                status : txExtraData.status,
-                transactionType : utils.returnCommand(tx.fusionCommand),
-                hash : tx.hash,
-                block : tx.height,
-                age : `${format(tx.timeStamp * 1000)} (${moment(tx.timeStamp * 1000).format('ll')})`,
-                gasUsed : parseInt(txExtraData.gasUsed,16),
-                nonce : parseInt(txTransactionData.nonce,16),
-                inputData : txTransactionData.input
+                from: tx.fromAddress,
+                to: tx.commandExtra3,
+                asset: '',
+                status: txExtraData.status,
+                transactionType: utils.returnCommand(tx.fusionCommand),
+                hash: tx.hash,
+                block: tx.height,
+                age: `${format(tx.timeStamp * 1000)} (${moment(tx.timeStamp * 1000).format('ll')})`,
+                gasUsed: parseInt(txExtraData.gasUsed, 16),
+                nonce: parseInt(txTransactionData.nonce, 16),
+                inputData: txTransactionData.input
             };
         });
 
-        if( data.transactionType == 'Send Asset' ||
+        if (data.transactionType == 'Send Asset' ||
             data.transactionType == 'Time Lock To Asset' ||
             data.transactionType == 'Asset To Time Lock' ||
-            data.transactionType == 'Time Lock To Time Lock'){
+            data.transactionType == 'Time Lock To Time Lock') {
             let asset = {};
-            await window.getAsset(txExtraData2.AssetID).then(function(r){
+            await window.getAsset(txExtraData2.AssetID).then(function (r) {
                 asset = r;
             });
 
             let amount = new BigNumber(txExtraData2.Value.toString());
-            let amountFinal = amount.div($scope.countDecimals(asset["Decimals"]));
+            let amountFinal = amount.div($scope.countDecimals(asset['Decimals']));
             data.asset_id = txExtraData2.AssetID;
-            data.asset_symbol = `${asset["Symbol"]}`;
+            data.asset_symbol = `${asset['Symbol']}`;
             data.amount = amountFinal.toString();
-            data.asset = `${asset["Name"]} (${asset["Symbol"]})`;
-            if(txExtraData2.StartTime){
-                data.timelock = `${$scope.returnDateString(txExtraData2.StartTime,'Start')} - ${$scope.returnDateString(txExtraData2.EndTime,'End')}`;
+            data.asset = `${asset['Name']} (${asset['Symbol']})`;
+            if (txExtraData2.StartTime) {
+                data.timelock = `${$scope.returnDateString(txExtraData2.StartTime, 'Start')} - ${$scope.returnDateString(txExtraData2.EndTime, 'End')}`;
             }
         }
-        if(data.transactionType == 'Create Asset'){
-            console.log(txExtraData2);
-            data.asset = `${txExtraData2["Name"]} (${txExtraData2["Symbol"]})`;
+        if (data.transactionType == 'Create Asset') {
+            data.asset = `${txExtraData2['Name']} (${txExtraData2['Symbol']})`;
             data.asset_id = txExtraData2.AssetID;
             console.log(data);
         }
-        $scope.$apply(function(){
+        if (data.transactionType == 'Make Swap') {
+            console.log(txExtraData2);
+            let fromAsset = {};
+            let toAsset = {};
+            await window.getAsset(txExtraData2.FromAssetID).then(function (r) {
+                fromAsset = r;
+                console.log(fromAsset);
+            });
+            await window.getAsset(txExtraData2.ToAssetID).then(function (r) {
+                toAsset = r;
+                console.log(toAsset);
+            })
+            data.swap = true;
+            data.fromSwap = fromAsset.Symbol;
+            data.toSwap = toAsset.Symbol;
+        }
+        $scope.$apply(function () {
             $scope.transactionData = data;
         });
     };
