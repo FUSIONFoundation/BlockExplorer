558246f2aba30f8a9da3941544a2a21dd9a52e03
Joey Kheireddine <joey.kheireddine@outlook.com>
2019-07-12 01:28:49 +0200

    Finished translation of Blocks page

--- app/js/translations/nl.js
+++ app/js/translations/nl.js
@@ -22,12 +22,24 @@ nl.data = {
     DASHBOARD_TRANSACTIONS : 'Transacties',
     DASHBOARD_ASSETS : 'Assets',
     DASHBOARD_TICKETS : 'Loten',
-
     DASHBOARD_LATESTBLOCKS : 'Laatste Blokken',
     DASHBOARD_VIEWALL : 'Bekijk alles',
     DASHBOARD_MINEDBY : 'GEMINED DOOR',
     DASHBOARD_LATESTTRANSACTIONS : 'Laatste Transacties',
 
+    // BLOCKS PAGE
+
+    BLOCKS_BLOCKS : 'Blokken',
+    BLOCKS_GOTOBLOCK : 'Ga naar blok',
+    BLOCKS_ROWS : 'Rijen',
+    BLOCKS_OF : 'van',
+    BLOCKS_BLOCK : 'Blok',
+    BLOCKS_AGE : 'Leeftijd',
+    BLOCKS_TRANSACTIONS : 'Transacties',
+    BLOCKS_MINER : 'Miner',
+    BLOCKS_GAS_USED : 'Gas Verbruikt',
+    BLOCKS_GAS_LIMIT : 'Gas Limiet',
+    BLOCKS_REWARD : 'Beloning',
 
 
 };
