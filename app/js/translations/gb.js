1e8bddcedc724883f5fcb42c8ec2716f5de49af8
Joey Kheireddine <joey.kheireddine@outlook.com>
2019-07-12 14:34:46 +0200

    Finished gb as fallback language

--- app/js/translations/gb.js
+++ app/js/translations/gb.js
@@ -1,8 +1,153 @@
 // Korean
 'use strict';
-var en = function() {}
-en.data = {
-    NAVBAR_NETWORKS : 'NETWORKS'
+var gb = function () {
+}
+gb.data = {
+
+    // PLEASE MAKE USE OF SAME CAPITALIZATION
+
+    // NAVBAR
+
+    NAVBAR_NETWORKS: 'NETWORKS',
+    NAVBAR_DASHBOARD: 'Dashboard',
+    NAVBAR_BLOCKS: 'Blocks',
+    NAVBAR_TRANSACTIONS: 'Transactions',
+    NAVBAR_ASSETS: 'Assets',
+    NAVBAR_ADDRESSES: 'Addresses',
+    NAVBAR_STAKING: 'Staking',
+    NAVBAR_SEARCH: 'Search by Address / TxHash / Block',
+
+    // DASHBOARD PAGE
+
+    DASHBOARD_LOADING: 'Loading',
+    DASHBOARD_PRICE: 'Price',
+    DASHBOARD_MARKETCAP: 'Market Cap',
+    DASHBOARD_BLOCKHEIGHT: 'Block Height',
+    DASHBOARD_TRANSACTIONS: 'Transactions',
+    DASHBOARD_ASSETS: 'Assets',
+    DASHBOARD_TICKETS: 'Tickets',
+    DASHBOARD_LATESTBLOCKS: 'Latest Blocks',
+    DASHBOARD_VIEWALL: 'View All',
+    DASHBOARD_MINEDBY: 'MINED BY',
+    DASHBOARD_LATESTTRANSACTIONS: 'Latest Transactions',
+
+    // BLOCKS PAGE
+
+    BLOCKS_BLOCKS: 'Blocks',
+    BLOCKS_GOTOBLOCK: 'Go to block',
+    BLOCKS_ROWS: 'Rows',
+    BLOCKS_OF: 'of',
+    BLOCKS_BLOCK: 'Block',
+    BLOCKS_AGE: 'Age',
+    BLOCKS_TRANSACTIONS: 'Transactions',
+    BLOCKS_MINER: 'Miner',
+    BLOCKS_GAS_USED: 'Gas Used',
+    BLOCKS_GAS_LIMIT: 'Gas Limit',
+    BLOCKS_REWARD: 'Reward',
+
+    // BLOCK PAGE
+
+    BLOCK_BLOCKHEIGHT: 'Block Height',
+    BLOCK_TRANSACTIONS: 'Transactions',
+    BLOCK_AGE: 'Age',
+    BLOCK_REWARD: 'Reward',
+    BLOCK_HASH: 'Hash',
+    BLOCK_PARENTHASH: 'Parent Hash',
+    BLOCK_MINER: 'Miner',
+    BLOCK_DIFFICULTY: 'Difficulty',
+    BLOCK_TOTAL_DIFFICULTY: 'Total Difficulty',
+    BLOCK_SIZE: 'Size',
+    BLOCK_GAS_USED: 'Gas Used',
+    BLOCK_GAS_LIMIT: 'Gas Limit',
+    BLOCK_TRANSACTION_HASH: 'Transaction Hash',
+    BLOCK_BLOCK: 'Block',
+    BLOCK_TYPE: 'Type',
+    BLOCK_NONCE: 'Nonce',
+    BLOCK_ASSETS: 'Assets',
+    BLOCK_FEES: 'Fees',
+
+    // TRANSACTIONS PAGE
+
+    TRANSACTIONS_TRANSACTIONS: 'Transactions',
+    TRANSACTIONS_GO_TO_TRANSACTION: 'Go to transaction',
+    TRANSACTIONS_ROWS: 'Rows',
+    TRANSACTIONS_OF: 'of',
+    TRANSACTIONS_TRANSACTION_HASH: 'Transaction Hash',
+    TRANSACTIONS_BLOCK: 'Block',
+    TRANSACTIONS_AGE: 'Age',
+    TRANSACTIONS_TYPE: 'Type',
+    TRANSACTIONS_FEES: 'Fees',
+
+    // TRANSACTION PAGE
+
+    TRANSACTION_NOT_EMITTED_MESSAGE: 'This transaction was not processed or was not emitted to the blockchain',
+    TRANSACTION_RETRYING_IN: 'Retrying in',
+    TRANSACTION_SECONDS: 'seconds',
+    TRANSACTION_TRANSACTION: 'Transaction',
+    TRANSACTION_TIME_LOCK: 'Time Lock',
+    TRANSACTION_SWAP_DETAILS: 'Swap Detials',
+    TRANSACTION_FROM: 'From:',
+    TRANSACTION_TO: 'To:',
+    TRANSACTION_ASSET: 'Asset:',
+    TRANSACTION_STATUS: 'Status',
+    TRANSACTION_TRANSACTION_TYPE: 'Transaction Type',
+    TRANSACTION_TRANSACTION_HASH: 'Transaction Hash',
+    TRANSACTION_BLOCK: 'Block',
+    TRANSACTION_AGE: 'Age',
+    TRANSACTION_GAS_USED: 'Gas Used',
+    TRANSACTION_NONCE: 'Nonce',
+
+    // ASSETS PAGE
+
+    ASSETS_ASSETS: 'Assets',
+    ASSETS_ASSET_ID: 'Asset ID',
+    ASSETS_ASSET_NAME: 'Asset Name',
+    ASSETS_QUANTITY: 'Quantity',
+    ASSETS_OF: 'of',
+    ASSETS_ROWS: 'Rows',
+    ASSETS_SEARCH_ASSET: 'Search assets',
+
+    // ASSETS PAGE
+
+    ASSET_ASSET: 'Asset',
+    ASSET_ASSET_NAME: 'Asset Name',
+    ASSET_ASSET_SYMBOL: 'Asset Symbol',
+    ASSET_TYPE: 'Asset Type',
+    ASSET_ISSUED_ON_FUSION: 'Issued on FUSION',
+    ASSET_DECIMALS: 'Decimals',
+    ASSET_TOTAL_SUPPLY: 'Total Supply',
+    ASSET_ISSUER: 'Issuer',
+    ASSET_ISSUE_HEIGHT: 'Issue Height',
+    ASSET_CHANGEABLE_SUPPLY: 'Changeable Supply',
+    ASSET_YES: 'Yes',
+    ASSET_NO: 'No',
+
+    // STAKING PAGE
+
+    STAKING_STAKING: 'Staking',
+    STAKING_LAST_UPDATED: 'Last updated',
+    STAKING_STAKING_STATUS: 'Staking Status',
+    STAKING_ACTIVE: 'Active',
+    STAKING_TICKETS: 'Tickets',
+    STAKING_REWARDS_TO_DATE: 'Rewards to Date',
+    STAKING_ADDRESS: 'Address',
+    STAKING_WALLET_ADDRESS: 'Wallet Address',
+    STAKING_INPUT_FIELD: 'Your wallet address starting with 0x',
+    STAKING_INFORMATION: 'In order to start tracking your staking rewards, please fill in your wallet address. Feel free to come back to this page at any time to resume tracking.',
+
+    // FOOTER PAGE
+
+    FOOTER_INFORMATION: 'FUSION Block Explorer is a tool developed to track all transactions on the blockchain.',
+    FOOTER_FUSION_TOOLS: 'FUSION Tools',
+    FOOTER_MORE: 'MORE',
+    FOOTER_FUSION_NETWORK: 'FUSION Network',
+    FOOTER_FSN_TOKEN: 'FSN Token',
+    FOOTER_ABOUT_US: 'About Us',
+    FOOTER_SUPPORT: 'Support',
+    FOOTER_CONTACT: 'Contact',
+    FOOTER_FAQ: 'FAQ',
+    FOOTER_SUBMIT_AN_ISSUE: 'Submit an Issue',
+    FOOTER_ALL_RIGHTS_RESERVED: 'All Rights Reserved'
 };
 
-module.exports = en.data;
+module.exports = gb.data;
