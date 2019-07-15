a4039e8c6d59cc294f1d729f0111cbaebe033c65
Joey Kheireddine <joey.kheireddine@outlook.com>
2019-07-15 19:46:43 +0200

    Added fsn.allTicketsByAddress to staking page

--- app/js/staking/controllers/controller.js
+++ app/js/staking/controllers/controller.js
@@ -7,12 +7,31 @@ let blockController = function ($http, $scope) {
     };
     $scope.walletAddress = $scope.walletAddressSaved;
     $scope.lastUpdated = moment(new Date().getTime()).format('LTS');
+    $scope.activeTickets = [];
 
     $scope.$watch('walletAddressSaved', function () {
         if ($scope.walletAddressSaved !== '' || $scope.walletAddressSaved === undefined) {
             $scope.getStakingInfo($scope.walletAddressSaved);
         }
-    })
+    });
+
+    $scope.returnDateString = function (posixtime, position) {
+        let time = new Date(parseInt(posixtime) * 1000);
+        if (posixtime == 18446744073709552000 && position == "End") {
+            return "Forever";
+        }
+        if (position == "Start") {
+            if (posixtime == 0) {
+                return "Now";
+            }
+            // if(posixtime < time && position == 'Start'){return 'Now';}
+        }
+        let tMonth = time.getUTCMonth();
+        let tDay = time.getUTCDate();
+        let tYear = time.getUTCFullYear();
+
+        return window.months[tMonth] + " " + tDay + ", " + tYear;
+    };
 
     $scope.saveCookie = function () {
         // console.log(window.web3.utils.isAddress($scope.walletAddress))
@@ -29,7 +48,7 @@ let blockController = function ($http, $scope) {
         localStorage.setItem('stakingAddress', '')
     };
 
-    $scope.getStakingInfo = function (walletAddress) {
+    $scope.getStakingInfo = async function (walletAddress) {
         if (!walletAddress) {
             return
         }
@@ -37,7 +56,7 @@ let blockController = function ($http, $scope) {
             let data = {};
             let ticketsData;
             let tickets = {};
-            $http.get(`${window.getServer()}balances/${walletAddress}`).then(function (r) {
+            await $http.get(`${window.getServer()}balances/${walletAddress}`).then(function (r) {
                 data = r.data;
                 console.log(data);
                 if (data.length === 0) {
@@ -54,9 +73,33 @@ let blockController = function ($http, $scope) {
                     $scope.stakingRewardsEarned = data[0].rewardEarn;
                     $scope.totalTickets = tickets.length;
                 }
+                $scope.getActiveTickets(walletAddress);
                 $scope.lastUpdated = moment(new Date().getTime()).format('LTS');
-
             });
+        } catch (err) {
+            // console.log(err)
+        }
+    };
+    $scope.getActiveTickets = async function (walletAddress) {
+        $scope.activeTickets = [];
+        let activeTickets = []
+        if (!walletAddress) {
+            return
+        }
+        try {
+          await window.web3.fsn.allTicketsByAddress(walletAddress).then(function(tickets){
+             for(let ticket in tickets){
+                 let data = {
+                     block : tickets[ticket].Height,
+                     timespan : `${$scope.returnDateString(tickets[ticket].StartTime,'Start')} - ${$scope.returnDateString(tickets[ticket].ExpireTime,'End')}`,
+                     value : '5000 FSN',
+                 }
+                 activeTickets.push(data);
+             }
+             $scope.$apply(function(){
+                 $scope.activeTickets = activeTickets;
+             })
+          });
         } catch (err) {
             // console.log(err)
         }
